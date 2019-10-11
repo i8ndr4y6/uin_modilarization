a9cc5ad84d680dcee48ce74f7ce7b2bdd4a3f21c
i8ndr4y6 <56153923+i8ndr4y6@users.noreply.github.com>
2019-10-11 16:42:37 +0700

    kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,12 @@
+nama = 'indryanti syam'
+program = 'gerak lurus'
+
+print(f'program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak/waktu
+
+print (f'jarak = {jarak/1000}km di tempuh dalam waktu = {waktu/60}menit')
+print (f'sehingga kecepatan = {kecepatan} m/s')
+
